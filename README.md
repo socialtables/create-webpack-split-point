fb7dfeb9fc52aa556d578f14badd5d9736acbb0e
Conor Hastings <hastings.conorm@gmail.com>
2016-12-05 20:48:40 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# create-lazy-bundle-component
+
+Webpack 2 exposes System.import a async module loader that will automatically define a split point and create a new bundle in your code that will only be loaded if the system.import is executed. React components that are not part of initial paint are a taylor made use case for this. This module aims to abstract a way the work of creating one of these components.
+
+## use
+
+`npm install -g @socialtables/create-lazy-bundle-component`;
+
+Then in terminal call the module with --path and a path to the component file. This will create 2 files in the directory of the component. In the case below it would create `./components/header/header-async.js` and `./components/header/index.js`.
+
+```
+create-lazy-bundle-component --path ./components/header/header.js
+```
+
+Then in the place you want to use the async header component you could simply import from the directory.
+
+```js
+import Header from "./components/header";
+```
+
+As soon as componentDidMount in Header is fired `System.import` will be called and it will asynchronously load the bundle containing the Header component and render it on the page, before that a Component returning null will be rendered.
---
+++ index.js
@@ -0,0 +1,38 @@
+"use strict";
+const fs = require("fs");
+
+module.exports = function createLazyBundleComponent(path) {
+	const pathSplit = path.split("/");
+	const fileName = pathSplit[pathSplit.length - 1].split(".js")[0];
+	const newFileName = `${fileName}-async.js`;
+	const component = `const DefaultComponent = () => null;
+export default class ${fileName[0].toUpperCase()}${fileName.slice(1, fileName.length)}Async extends React.Component {
+	constructor() {
+		super();
+		this.state = { Component: DefaultComponent };
+	}
+	componentDidMount() {
+		System.import(path).then(file => this.setState({ Component: file.default }));
+	}
+	render() {
+		const { Component } = this.state;
+		return <Component {...this.props} />;
+	}
+}
+`;	
+	const index = `export { default } from "./${newFileName}";`;
+	fs.writeFile(pathSplit.slice(0, pathSplit.length - 1).concat(newFileName).join("/"), component, err => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		fs.writeFile(pathSplit.slice(0, pathSplit.length - 1).concat("index.js").join("/"), index, err => {
+			if (err) {
+				console.error(err);
+				process.exit(1);
+			}
+			console.log("Succesfully created async component files");
+			process.exit(0);
+		});
+	});
+}
